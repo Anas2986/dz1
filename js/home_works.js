798e4af96adec303bb14432bccde697920fb6cb3
Anas2986 <anassajfiev634@gmail.com>
2024-05-11 20:31:53 +0600

    dz2

--- js/home_works.js
+++ js/home_works.js
@@ -15,12 +15,52 @@ gmailBtn.onclick=()=>{
 
 const childBlock =document.querySelector('.child_block')
 let positionX =0
+let positionY =0
 let width =449
 const moveBlock =()=>{
     childBlock.style.left=`${positionX}px`
-    if (positionX<width){
+    childBlock.style.top=`${positionY}px`
+    if (positionX<width && positionY===0){
         positionX++
+        setTimeout(moveBlock,4)
+    }else if (positionX===width && positionY<width){
+        positionY++
+        setTimeout(moveBlock,4)
+    }else if (positionY===width && positionX !==0){
+        positionX--
+        setTimeout(moveBlock,4)
+    }else if (positionX ===0 && positionY !==0){
+        positionY--
         setTimeout(moveBlock,4)
     }
 }
 moveBlock()
+const stopButton =document.querySelector('#stop')
+const startButton =document.querySelector('#start')
+const resetButton =document.querySelector('#reset')
+const secondsButton =document.querySelector('#seconds')
+
+let count =0
+let interval ;
+let timerRunning=false
+
+startButton.onclick=()=>{
+    if (timerRunning===false){
+        interval=setInterval(()=>{
+            count++
+            secondsButton.textContent=count
+        },1000)
+        timerRunning = true
+    }
+}
+
+stopButton.onclick=()=>{
+    clearInterval(interval )
+    timerRunning = false
+}
+resetButton.onclick=() =>{
+    clearInterval(interval)
+    count= 0
+    secondsButton.textContent=count
+    timerRunning= false
+}
